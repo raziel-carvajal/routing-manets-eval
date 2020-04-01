5deeeb68c4057604ede0f20620787be9a56dc40c
raziel-carvajal <raziel.carvajal@gmail.com>
2020-04-01 18:48:51 +0200

    adds to gitignore and minor updates

--- bin/run-config.sh
+++ bin/run-config.sh
@@ -19,22 +19,20 @@
 endStr="End of ${0}"
 # check correct # of arguments
 [ ${#} -ne 3 ] && \
-  echo -e "Usage: ${0} INET_PATH INI_FILE CONFIGURATION\n${endStr}"  && exit 1
+  echo -e "Usage: ${0} OMNET_PATH INI_FILE CONFIG_TO_RUN\n${endStr}"  && exit 1
 # check that the INI file exist
 [ ! -f ${2} ] && \
   echo -e "Error: configuration (*.ini) file doesn't exist.\n${endStr}" && exit 1
 # check that INI file contains the written configuration
 grep ${3} ${2}
 [ ${?} -ne 0 ] && \
-  echo -e "Error: experiment doesn't exist in ini file.\n${endStr}" && exit 1
+  echo -e "Error: configuration (${3}) doesn't exist in INI file.\n${endStr}" && exit 1
 
 # set environment for omnet++/inet
-myNedFiles=$(cd .. && pwd)
-source set-env.sh ${1}
+source bin/set-env.sh ${1}
 [ ${?} -ne 0 ] && \
   echo -e "Error. Cannot set environment for omnet++/inet.\n${endStr}" && exit 1
 
-opp_run -u Cmdenv -n ${myNedFiles}/networks:${INET_NED_PATH} \
-  -l ${INET_ROOT}/src/INET -c ${3} -f ${2}
+opp_run -u Cmdenv -n networks/built:${INET_NED_PATH} -l ${INET_ROOT}/src/INET -c ${3} -f ${2}
 
 echo "Ok, ${0} ends successfully."
