3936efa53682fb66a010da9cede0c0ea6c76df6c
raziel-carvajal <raziel.carvajal@gmail.com>
2020-05-16 17:16:03 +0200

    plot several datasets to assez the performance of routing algorithms

---
+++ src/plotting/plot_routing_algo_dataset.R
@@ -0,0 +1,113 @@
+library(argparse)
+
+source("common_funcs.R")
+
+parseArgs <- function() {
+  p <- ArgumentParser(
+    description='Plot several metrics to evaluate the performance of routing algorithms.'
+  )
+  # mandatory arguments
+  p$add_argument('high_density_dataset', type='character')
+  p$add_argument('low_density_dataset', type='character')
+  p$add_argument('protocol', type='character')
+  # options
+  p$add_argument('--with-arp-latency', dest='wal', action='store_true')
+  p$add_argument('--with-route-discovery-latency', dest='wrdl', action='store_true')
+  p$add_argument('--with-msg-latency', dest='wmela', action='store_true')
+  p$add_argument('--with-msg-loss', dest='wmelo', action='store_true')
+
+  p$parse_args()
+}
+args <- parseArgs()
+
+sparseDs <- read.table(args$low_density_dataset, header=TRUE)
+denseDs  <- read.table(args$high_density_dataset, header=TRUE)
+names(sparseDs) <- c('data', 'group') ; names(denseDs) <- c('data', 'group')
+
+df <- data.frame(
+  data=c( sparseDs$data, denseDs$data ),
+  group=as.factor( c( sparseDs$group, denseDs$group ) ),
+  density=c( rep('sparse', length(sparseDs$data)), rep('dense', length(denseDs$data)) )
+)
+df <- df[ order(df$group) ,]
+
+if (args$wal) {
+  info <- data.frame(
+    title=paste("Latency of ARP session in", args$protocol, "(reactive) protocol"),
+    xlabel="Number of hops between source and destination",
+    ylabel="Latency (ms)",
+    ylim=60
+  )
+  plotDistribGroups(df, info, withBoxplot=TRUE)
+}
+
+if (args$wmela) {
+  info <- data.frame(
+    title=paste("Latency of received messages in", args$protocol, "(reactive) protocol"),
+    xlabel="Number of hops between source and destination",
+    ylabel="Latency (ms)",
+    ylim=3000
+  )
+  plotDistribGroups(df, info, withBoxplot=TRUE)
+}
+
+if (args$wmelo) {
+  sparseDs <- read.table(args$low_density_dataset, header=TRUE)
+  denseDs  <- read.table(args$high_density_dataset, header=TRUE)
+  names(sparseDs) <- c('sent', 'received', 'hops')
+  names(denseDs) <- c('sent', 'received', 'hops')
+
+  sparseDs <- sparseDs[ order(sparseDs$hops), ]
+  denseDs <- denseDs[ order(denseDs$hops), ]
+
+  sparse <- lapply(unique( c(sparseDs$hops) ), function(h){
+    temp <- sparseDs[ sparseDs$hops == h, ]
+    data.frame(
+      data = sum(temp$received) / sum(temp$sent) * 100,
+      group = as.factor( c(h) ),
+      density = as.factor( c("sparse") )
+    )
+  })
+  sparse <- do.call("rbind", sparse)
+
+  dense <- lapply(unique( c(denseDs$hops) ), function(h){
+    temp <- denseDs[ denseDs$hops == h, ]
+    data.frame(
+      data = sum(temp$received) / sum(temp$sent) * 100,
+      group = as.factor( c(h) ),
+      density = as.factor( c("dense") )
+    )
+  })
+  dense <- do.call("rbind", dense)
+
+  df <- do.call("rbind", list(sparse, dense))
+
+  info <- data.frame(
+    title=paste("Message delivery fraction in", args$protocol, "(reactive) protocol"),
+    xlabel="Number of hops between source and destination",
+    ylabel="Delivery fraction (%)",
+    ylim=100
+  )
+  plotColumns(df, info)
+}
+
+if (args$wrdl) {
+  # info <- data.frame(
+  #   title=paste(
+  #     "Route discovery latency in", args$protocol, "(reactive) protocol",
+  #     "\n(the number of hops between source and destination varies in the range [4, 10]) "
+  #   ),
+  #   xlabel="Latency (ms)",
+  #   ylabel="CDF of route discovery latency (%)"
+  # )
+  # plotCDFset(df, info)
+  info <- data.frame(
+    title=paste(
+      "Route discovery latency in", args$protocol, "(reactive) protocol",
+      "\n(the number of hops between source and destination varies in the range [4, 10]) "
+    ),
+    xlabel="Number of hops",
+    ylabel="Latency (ms)"
+  )
+  plotDistribGroups(df, info)
+}
