07f0d041505475a1eda8ccbd429b64d11e1c381a
raziel-carvajal <raziel.carvajal@gmail.com>
2020-05-16 17:10:43 +0200

    legends in plot that presents nodes' degree

--- src/plotting/plot_density_static_net.R
+++ src/plotting/plot_density_static_net.R
@@ -8,26 +8,18 @@ parseArgs <- function() {
   )
   # mandatory arguments
   p$add_argument('dataset', type='character')
-  p$add_argument('plot_name', type='character')
 
   p$parse_args()
 }
 args <- parseArgs()
 
 info <- data.frame(
-  title="title",
-  xlabel="# of neighbors per node",
-  ylabel="CDF in % (nodes)"
+  title="Nodes degree in dense and sparse scenarios",
+  xlabel="Nodes degree",
+  ylabel="CDF of nodes degree (%)"
 )
 
 ds <- read.csv(args$dataset, header=TRUE)
-typ<- lapply(names(ds), function(col) {
-  rep(col, length(ds[ , col]))
-})
+ds$density <- as.factor(ds$density)
 
-df <- data.frame(
-  data = unlist(ds, use.names=FALSE),
-  Density= unlist(typ, use.names=FALSE)
-)
-
-plotCDFset(df, info)
+plotCDFset(ds, info)
